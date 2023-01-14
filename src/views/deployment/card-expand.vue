a0cbeb3fdcc604224c1dd5678c674d96f26d4888
googs1025 <googs1025@gmail.com>
2023-01-14 10:28:22 +0800

    feat: 新增deployment yaml功能

---
+++ src/views/deployment/card-expand.vue
@@ -0,0 +1,38 @@
+<template>
+  <i :class="icon"  @click="setExpand" ></i>
+</template>
+<script>
+export default {
+  props:{
+    expand:Boolean
+  },
+  data(){
+    return {
+      icon:'el-icon-arrow-up',
+      childExpand:this.expand,
+    }
+  },
+  mounted(){
+    this.setIcon()//设置图标
+  },
+  watch:{
+    childExpand:function (newValue,oldValue) {
+
+      this.$emit("update:expand",newValue)
+    }
+  },
+  methods:{
+    setIcon(){
+      if(this.childExpand){
+        this.icon='el-icon-arrow-up'
+      }else{
+        this.icon='el-icon-arrow-down'
+      }
+    },
+    setExpand(){
+      this.childExpand=!this.childExpand
+      this.setIcon()
+    }
+  }
+}
+</script>
